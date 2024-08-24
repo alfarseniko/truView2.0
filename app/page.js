96736d393f8a567ccc987c0df728b57964af4b28
alfarseniko <alfarseniko@gmail.com>
2024-08-24 19:15:49 +0500

    landing page draft complete, deploying

--- app/page.js
+++ app/page.js
@@ -26,8 +26,9 @@ export default function LandingPage() {
       {/* ----- HERO ----- */}
       
       <section class="relative">
-      <div class="bg-none lg:bg-civil-3 lg:bg-center pb-48">
-        <div class="container flex flex-col items-center lg:flex-row lg:justify-items-end gap-12 pt-14 lg:pt-28 ">
+      <div class="lg:bg-none bg-civil-3 bg-center pb-64 lg:hidden"></div>
+      <div class="bg-none lg:bg-civil-3 lg:bg-center lg:pb-48 pb-24">
+        <div class="container flex flex-col items-center lg:flex-row lg:justify-items-end gap-12 pt-14 lg:pt-64 lg:pb-64 ">
           {/* ----- Image ----- */}
           {/* <div class="flex justify-center flex-1 mb-10 md:mb-16 lg:mb-0 z-10">
             <img class="w-5/6 h-5/6 sm:w-3/4 sm:h3/4 md:w-full md:h-full" src="/images/hero-bg.png" alt="" />
@@ -51,13 +52,15 @@ export default function LandingPage() {
         </div> */}
         </div>
       </section>
+
       {/* ----- FEATURES ----- */}
-      <section class="bg-bookmark-white py-20 mt:20 lg:mt-60 md:mt-10">
+
+      <section class="bg-bookmark-white py-20 ">
         {/* ----- Heading ----- */}
         <div class="sm:w-3/4 lg:w-5/12 mx-auto px-2">
           <h1 class="text-3xl text-center text-bookmark-blue ">Features</h1>
           <p class="text-center text-bookmark-grey mt-4 ">
-            Manage your bookmarks effortlessly with our simple bookmark manager. Organize your favorite websites, categorize them with tags, and easily access them whenever you need.
+          Our platform leverages the power of blockchain technology to ensure that every project is managed with unparalleled transparency, efficiency, and security. Say goodbye to disputes, inefficiencies, and trust issues—TruView is here to transform how you build, one project at a time.
           </p>
         </div>
         {/* ----- Feature # 1 ----- */}
@@ -70,17 +73,16 @@ export default function LandingPage() {
             {/* ----- Content ----- */}
             <div class="flex flex-1 flex-col items-center lg:items-start">
               <h1 class="text-bookmark-blue text-3xl md:text-4 lg:text-5xl text-center lg:text-left mb-6">
-                Bookmark in One Click
+              Blockchain-Backed BIM Integration
               </h1>
               <p class="text-bookmark-grey my-4 text-lg text-center lg:text-left sm:w-3/4 lg:w-full">
-                Manage your bookmarks effortlessly with our simple bookmark manager. Organize your favorite websites, categorize them with tags, and easily access them whenever you need.
-              </p>
+              Seamlessly combine the robust transparency of blockchain with the precision of BIM. TruView ensures every detail of your project is securely recorded and tamper-proof.</p>
               <div class="flex justify-center flex-wrap gap-6">
-                <button type="button" class="btn btn-purple hover:bg-bookmark-white hover:text-black">More Info</button>
+                <button type="button" class="btn btn-zinc hover:bg-bookmark-white hover:text-black">More Info</button>
               </div>
             </div>
             {/* ----- ROUNDED RECTANGLE ----- */}
-            <div class="hidden lg:block overflow-hidden bg-bookmark-purple rounded-r-full absolute h-80 w-2/4 lg:-bottom-24 lg:-left-36">
+            <div class="hidden lg:block overflow-hidden bg-zinc-500 rounded-r-full absolute h-80 w-2/4 lg:-bottom-24 lg:-left-36">
             </div>
           </div>
         </div>
@@ -94,17 +96,16 @@ export default function LandingPage() {
             {/* ----- Content ----- */}
             <div class="flex flex-1 flex-col items-center lg:items-start">
               <h1 class="text-bookmark-blue text-3xl md:text-4 lg:text-5xl text-center lg:text-left mb-6">
-                Intelligent Search
+              Efficient Dispute Resolution
               </h1>
               <p class="text-bookmark-grey my-4 text-lg text-center lg:text-left sm:w-3/4 lg:w-full">
-                Manage your bookmarks effortlessly with our simple bookmark manager. Organize your favorite websites, categorize them with tags, and easily access them whenever you need.
-              </p>
+              Resolve disputes faster and more effectively with a transparent, verifiable trail of project activities. TruView provides a clear, indisputable record to streamline arbitration processes.</p>
               <div class="flex justify-center flex-wrap gap-6">
-                <button type="button" class="btn btn-purple hover:bg-bookmark-white hover:text-black">More Info</button>
+                <button type="button" class="btn btn-zinc hover:bg-bookmark-white hover:text-black">More Info</button>
               </div>
             </div>
             {/* ----- ROUNDED RECTANGLE ----- */}
-            <div class="hidden lg:block overflow-hidden bg-bookmark-purple rounded-l-full absolute h-80 w-2/4 lg:-bottom-24 lg:-right-36">
+            <div class="hidden lg:block overflow-hidden bg-zinc-500 rounded-l-full absolute h-80 w-2/4 lg:-bottom-24 lg:-right-36">
             </div>
           </div>
         </div>
@@ -118,33 +119,34 @@ export default function LandingPage() {
             {/* ----- Content ----- */}
             <div class="flex flex-1 flex-col items-center lg:items-start">
               <h1 class="text-bookmark-blue text-3xl md:text-4 lg:text-5xl text-center lg:text-left mb-6">
-                Share Your Bookmark
+              Enhanced Stakeholder Trust
               </h1>
               <p class="text-bookmark-grey my-4 text-lg text-center lg:text-left sm:w-3/4 lg:w-full">
-                Manage your bookmarks effortlessly with our simple bookmark manager. Organize your favorite websites, categorize them with tags, and easily access them whenever you need.
-              </p>
+              Build trust among all project participants with a platform that guarantees accountability and transparency, reducing risks and fostering a collaborative environment.</p>
               <div class="flex justify-center flex-wrap gap-6">
-                <button type="button" class="btn btn-purple hover:bg-bookmark-white hover:text-black">More Info</button>
+                <button type="button" class="btn btn-zinc hover:bg-bookmark-white hover:text-black">More Info</button>
               </div>
             </div>
             {/* ----- ROUNDED RECTANGLE ----- */}
-            <div class="hidden lg:block overflow-hidden bg-bookmark-purple rounded-r-full absolute h-80 w-2/4 lg:-bottom-24 lg:-left-36">
+            <div class="hidden lg:block overflow-hidden bg-zinc-500 rounded-r-full absolute h-80 w-2/4 lg:-bottom-24 lg:-left-36">
             </div>
           </div>
         </div>
       </section>
+
       {/* ----- DOWNLOAD ----- */}
-      <section class="py-20 mt-20">
+
+      {/* <section class="py-20 mt-20">
         <div class="sm:w-3/4 lg:w-5/12 mx-auto px-2">
           <h1 class="text-3xl text-center text-bookmark-blue ">Download</h1>
           <p class="text-center text-bookmark-grey mt-4 ">
             Manage your bookmarks effortlessly with our simple bookmark manager. Organize your favorite websites, categorize them with tags, and easily access them whenever you need.
           </p>
-        </div>
+        </div> */}
         {/* ----- Cards ----- */}
-        <div class="container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16 max-w-screen-lg mt-16">
+        {/* <div class="container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16 max-w-screen-lg mt-16">
           {/* ----- Card # 1 ----- */}
-          <div class="flex flex-col rounded-md shadow-md lg:mb-16">
+          {/* <div class="flex flex-col rounded-md shadow-md lg:mb-16">
             <div class="p-6 flex flex-col items-center">
               <img src="/images/logo-chrome.svg" alt="" />
               <h3 class="mt-5 mb-2 text-bookmark-blue text-lg">Add to Chrome</h3>
@@ -152,11 +154,11 @@ export default function LandingPage() {
             </div>
             <hr class="border-b border-bookmark-white"></hr>
             <div class="flex p-6">
-              <button type="button" class="flex-1 btn btn-purple hover:bg-bookmark-white hover:text-black">Add and Install Extension</button>
+              <button type="button" class="flex-1 btn btn-zinc hover:bg-bookmark-white hover:text-black">Add and Install Extension</button>
             </div>
-          </div>
+          </div> */}
           {/* ----- Card # 2 ----- */}
-          <div class="flex flex-col rounded-md shadow-md lg:my-8">
+          {/* <div class="flex flex-col rounded-md shadow-md lg:my-8">
             <div class="p-6 flex flex-col items-center">
               <img src="/images/logo-firefox.svg" alt="" />
               <h3 class="mt-5 mb-2 text-bookmark-blue text-lg">Add to Firefox</h3>
@@ -164,11 +166,11 @@ export default function LandingPage() {
             </div>
             <hr class="border-b border-bookmark-white"></hr>
             <div class="flex p-6">
-              <button type="button" class="flex-1 btn btn-purple hover:bg-bookmark-white hover:text-black">Add and Install Extension</button>
+              <button type="button" class="flex-1 btn btn-zinc hover:bg-bookmark-white hover:text-black">Add and Install Extension</button>
             </div>
-          </div>
+          </div> */}
           {/* ----- Card # 3 ----- */}
-          <div class="flex flex-col rounded-md shadow-md lg:mt-16">
+          {/* <div class="flex flex-col rounded-md shadow-md lg:mt-16">
             <div class="p-6 flex flex-col items-center">
               <img src="/images/logo-opera.svg" alt="" />
               <h3 class="mt-5 mb-2 text-bookmark-blue text-lg">Add to Opera</h3>
@@ -176,48 +178,60 @@ export default function LandingPage() {
             </div>
             <hr class="border-b border-bookmark-white"></hr>
             <div class="flex p-6">
-              <button type="button" class="flex-1 btn btn-purple hover:bg-bookmark-white hover:text-black">Add and Install Extension</button>
+              <button type="button" class="flex-1 btn btn-zinc hover:bg-bookmark-white hover:text-black">Add and Install Extension</button>
             </div>
           </div>
         </div>
-      </section>
+      </section> */}
+
       {/* ----- FAQ ----- */}
+
       <section class="bg-bookmark-white py-20">
         <div class="container">
           {/* ----- Header ----- */}
           <div class="sm:w-3/4 lg:w-5/12 mx-auto px-2">
             <h1 class="text-3xl text-center text-bookmark-blue ">Frequently Asked Questions</h1>
             <p class="text-center text-bookmark-grey mt-4 ">
-              Manage your bookmarks effortlessly with our simple bookmark manager. Organize your favorite websites, categorize them with tags, and easily access them whenever you need.
+            Everything You Need to Know About TruView
             </p>
           </div>
           {/* ----- FAQ Items ----- */}
           <div class="flex flex-col sm:w-3/4 lg:w-5/12 mt-12 mx-auto">
             <div class="flex items-center border-b py-4">
-              <span class="flex-1">What is a Bookmark?</span>
-              <i class="text-bookmark-purple fa-solid fa-chevron-down"></i>
+              <span class="flex-1">What is TruView?</span>
+              <i class="text-zinc-500 fa-solid fa-chevron-down"></i>
             </div>
             <div class="flex items-center border-b py-4">
-              <span class="flex-1">What is a Bookmark?</span>
-              <i class="text-bookmark-purple fa-solid fa-chevron-down"></i>
+              <span class="flex-1">How does TruView improve dispute resolution?</span>
+              <i class="text-zinc-500 fa-solid fa-chevron-down"></i>
             </div>
             <div class="flex items-center border-b py-4">
-              <span class="flex-1">What is a Bookmark?</span>
-              <i class="text-bookmark-purple fa-solid fa-chevron-down"></i>
+              <span class="flex-1">Can TruView be integrated with my current systems?</span>
+              <i class="text-zinc-500 fa-solid fa-chevron-down"></i>
             </div>
             <div class="flex items-center border-b py-4">
-              <span class="flex-1">What is a Bookmark?</span>
-              <i class="text-bookmark-purple fa-solid fa-chevron-down"></i>
+              <span class="flex-1">Is my data secure with TruView?</span>
+              <i class="text-zinc-500 fa-solid fa-chevron-down"></i>
             </div>
-            <button type="button" class="flex self-center mt-4 btn btn-purple hover:bg-bookmark-white hover:text-black">More Info</button>
+            <div class="flex items-center border-b py-4">
+              <span class="flex-1">Who can benefit from using TruView?</span>
+              <i class="text-zinc-500 fa-solid fa-chevron-down"></i>
+            </div>
+            <div class="flex items-center border-b py-4">
+              <span class="flex-1">How can I get started with TruView?</span>
+              <i class="text-zinc-500 fa-solid fa-chevron-down"></i>
+            </div>
+            <button type="button" class="flex self-center mt-4 btn btn-zinc hover:bg-bookmark-white hover:text-black">More Info</button>
           </div>
         </div>
       </section>
+
       {/* ----- Contact Us ----- */}
-      <section class="bg-bookmark-purple text-white py-20">
+
+      <section class="bg-zinc-700 text-white py-20">
         <div class="container">
           <div class="sm:w-3/4 lg:w-2/4 mx-auto">
-            <p class="font-light uppercase text-center mb-8">35,000+ ALREADY JOINED</p>
+            {/* <p class="font-light uppercase text-center mb-8">35,000+ ALREADY JOINED</p> */}
             <h1 class="text-3xl text-center"> Stay up-to-date with what we're doing</h1>
             <div class="flex flex-col sm:flex-row gap-6 mt-8">
               <input type="text" placeholder="Enter your email address" class="focus:outline-none flex-1 px-2 py-3 rounded-md text-black" />
@@ -226,11 +240,14 @@ export default function LandingPage() {
           </div>
         </div>
       </section>
+
       {/* ----- Footer ----- */}
-      <section class="bg-bookmark-blue py-8">
+      
+      <section class="bg-zinc-500 py-8">
         <div class="container flex flex-col md:flex-row items-center">
           <div class="flex flex-1 flex-wrap items-center justify-center md:justify-start gap-12">
-            <img src="/images/logo-bookmark-white.png" alt="" />
+            {/* <img src="/images/logo-bookmark-white.png" alt="" /> */}
+            <h1 class="font-bold text-white text-2xl pl-12">TRUVIEW</h1>
             <ul class="flex text-white uppercase gap-12 text-xs">
               <li class="cursor-pointer">Features</li>
               <li class="cursor-pointer">Pricing</li>
