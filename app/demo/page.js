86ffb4165adf028cd91fa068423ee0cbec8b47a9
Alfa Arseniko <alfarseniko@gmail.com>
2024-11-10 14:58:09 +0500

    just a comment

--- app/demo/page.js
+++ app/demo/page.js
@@ -1,27 +1,30 @@
-'use client'
+"use client";
 
-import Head from 'next/head'
-import * as OBC from 'openbim-components'
-import * as THREE from 'three'
+import Head from "next/head";
+import * as OBC from "openbim-components";
+import * as THREE from "three";
 
 export default function Demo() {
+  const viewer = new OBC.Components();
+  const sceneComponent = new OBC.SimpleScene(viewer);
+  viewer.scene = sceneComponent;
+  const scene = sceneComponent.get();
+  const ambientLight = new THREE.AmbientLight(0xe6e7e4, 1);
+  const directionalLight = new THREE.DirectionalLight(0xf9f9f9, 0.75);
+  directionalLight.position.set(10, 50, 10);
+  scene.add(ambientLight, directionalLight);
+  scene.background = new THREE.Color("#202932");
 
-    const viewer = new OBC.Components()
-    const sceneComponent = new OBC.SimpleScene(viewer)
-    viewer.scene = sceneComponent
-    const scene = sceneComponent.get()
-    const ambientLight = new THREE.AmbientLight(0xE6E7E4, 1)
-    const directionalLight = new THREE.DirectionalLight(0xF9F9F9, 0.75)
-    directionalLight.position.set(10, 50, 10)
-    scene.add(ambientLight, directionalLight)
-    scene.background = new THREE.Color('#202932')
-
-    return (
-        <html lang="en">
-            <Head>
-                <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
-            </Head>
-            <body></body>
-        </html>
-    )
+  return (
+    <html lang="en">
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          rel="stylesheet"
+        />
+      </Head>
+      <body></body>
+    </html>
+  );
 }
+//testing comment
